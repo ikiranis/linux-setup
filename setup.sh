fcdf5e38a9862e70c7d0a895517fd1c0d01f8ecc
Yiannis Kiranis <50238022+ikiranis@users.noreply.github.com>
2019-09-03 01:22:01 +0300

    Update comment

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 # Install albert
 # https://albertlauncher.github.io/docs/installing/
 
+# Install for Ubuntu 19.04
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_19.04/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list"
 wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_19.04/Release.key -O Release.key
 sudo apt-key add - < Release.key
