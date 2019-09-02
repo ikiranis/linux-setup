e0c146eb09e9de3e247c05c3dd5b99b6d42c097d
Yiannis Kiranis <50238022+ikiranis@users.noreply.github.com>
2019-09-03 01:37:12 +0300

    Update with basic programs installation

--- setup.sh
+++ setup.sh
@@ -1,9 +1,31 @@
+#!/bin/bash
+
 # Install albert
 # https://albertlauncher.github.io/docs/installing/
 
-# Install for Ubuntu 19.04
+# Add repositories
+
+# Repository for Albert, in Ubuntu 19.04
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_19.04/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list"
 wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_19.04/Release.key -O Release.key
 sudo apt-key add - < Release.key
-sudo apt-get update
-sudo apt-get install albert
+
+# Install programs
+sudo apt update
+
+# Basic programs/tools
+sudo apt install htop git albert vlc gnome-tweaks gnome-tweak-tool spell myspell-el-gr -y
+
+# Graphics
+sudo apt install gimp scribus -y
+
+# Games
+sudo apt install steam-installer -y
+
+# Remove junk
+sudo apt-get remove ubuntu-web-launchers thunderbird rhythmbox -y
+
+# Gotta reboot now:
+sudo apt update && sudo apt upgrade -y
+
+echo $'\n'$"*** All done! Please reboot now. ***"
