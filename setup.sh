51471921dc620b93d83d32141a0964a483bb47ce
Yiannis Kiranis <50238022+ikiranis@users.noreply.github.com>
2019-09-09 01:39:57 +0300

    Add timeshift installation

--- setup.sh
+++ setup.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-# Install albert
-# https://albertlauncher.github.io/docs/installing/
-
 # Add repositories
 
 # Repository for Albert, in Ubuntu 19.04
+# https://teejeetech.in/timeshift/
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_19.04/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list"
 wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_19.04/Release.key -O Release.key
 sudo apt-key add - < Release.key
+
+# Repository for Timeshift
+# https://teejeetech.in/timeshift/
+sudo add-apt-repository -y ppa:teejee2008/ppa
 
 # Install software
 sudo apt update
 
 # Basic software/tools
-sudo apt install htop git albert vlc gnome-tweaks gnome-tweak-tool spell myspell-el-gr -y
+sudo apt install htop git albert timeshift vlc gnome-tweaks gnome-tweak-tool spell myspell-el-gr -y
 
 # Graphics
 sudo apt install gimp -y
