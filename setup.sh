bf266de249d8d9e03d5249865f4283682d91da05
Yiannis Kiranis <50238022+ikiranis@users.noreply.github.com>
2019-09-09 16:40:44 +0300

    Fix link

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # Add repositories
 
 # Repository for Albert, in Ubuntu 19.04
-# https://teejeetech.in/timeshift/
+# https://albertlauncher.github.io/docs/installing/
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_19.04/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list"
 wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_19.04/Release.key -O Release.key
 sudo apt-key add - < Release.key
