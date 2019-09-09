147a0f8e63a8b28125a9f69b10e1f23c9ec4e471
Yiannis Kiranis <50238022+ikiranis@users.noreply.github.com>
2019-09-09 16:51:44 +0300

    Edit link

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # Add repositories
 
 # Repository for Albert
-# https://albertlauncher.github.io/docs/installing/
+# https://software.opensuse.org/download.html?project=home:manuelschneid3r&package=albert
 # @ Ubuntu 18.04
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list"
 wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_18.04/Release.key -O Release.key
