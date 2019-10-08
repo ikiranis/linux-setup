411c134ce44cd74d5308b49ceaf9f4213de87b46
Yiannis Kiranis <50238022+ikiranis@users.noreply.github.com>
2019-10-08 22:34:20 +0300

    pidgin

--- setup.sh
+++ setup.sh
@@ -5,11 +5,11 @@
 # Repository for Albert
 # https://software.opensuse.org/download.html?project=home:manuelschneid3r&package=albert
 # @ Ubuntu 18.04
-sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list"
-wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_18.04/Release.key -O Release.key
+# sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list"
+# wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_18.04/Release.key -O Release.key
 # @ Ubuntu 19.04
-# sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_19.04/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list"
-# wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_19.04/Release.key -O Release.key
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_19.04/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list"
+wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_19.04/Release.key -O Release.key
 sudo apt-key add - < Release.key
 
 # Repository for Timeshift
@@ -21,7 +21,7 @@ sudo apt update
 
 # Basic software/tools
 sudo apt install htop git albert timeshift vlc gnome-tweaks gnome-tweak-tool \
-gthumb spell gdebi myspell-el-gr guake nmap file-roller pavucontrol -y
+gthumb spell gdebi myspell-el-gr guake nmap file-roller pavucontrol pidgin -y
 
 # GSConnect
 # https://github.com/andyholmes/gnome-shell-extension-gsconnect/wiki
@@ -49,9 +49,9 @@ sudo apt install ranger -y
 # Install cronopete
 # http://www.rastersoft.com/programas/cronopete.html
 # for 18.04
-wget http://www.rastersoft.com/descargas/cronopete/cronopete-bionic_4.4.0-ubuntu1_amd64.deb
+# wget http://www.rastersoft.com/descargas/cronopete/cronopete-bionic_4.4.0-ubuntu1_amd64.deb
 # for 19.04
-# wget http://www.rastersoft.com/descargas/cronopete/cronopete-disco_4.11.0-ubuntu1_amd64.deb
+wget http://www.rastersoft.com/descargas/cronopete/cronopete-disco_4.11.0-ubuntu1_amd64.deb
 sudo gdebi cronopete*.deb
 
 # Remove junk
