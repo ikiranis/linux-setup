d6c060a5f4975cc265098e4921ea3b271e14ee0f
Yiannis Kiranis <50238022+ikiranis@users.noreply.github.com>
2019-10-08 22:26:58 +0300

    Pavucontrol

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ sudo apt update
 
 # Basic software/tools
 sudo apt install htop git albert timeshift vlc gnome-tweaks gnome-tweak-tool \
-gthumb spell gdebi myspell-el-gr guake nmap file-roller -y
+gthumb spell gdebi myspell-el-gr guake nmap file-roller pavucontrol -y
 
 # GSConnect
 # https://github.com/andyholmes/gnome-shell-extension-gsconnect/wiki
