bc72f0289643545ef402fa51bb448fcecceaefe1
Yiannis Kiranis <50238022+ikiranis@users.noreply.github.com>
2019-10-09 00:53:00 +0300

    gnome shell

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ sudo apt update
 
 # Basic software/tools
 sudo apt install htop git albert timeshift vlc gnome-tweaks gnome-tweak-tool \
-gthumb spell gdebi myspell-el-gr guake nmap file-roller pavucontrol pidgin -y
+gthumb spell gdebi myspell-el-gr guake nmap file-roller pavucontrol pidgin chrome-gnome-shell -y
 
 # GSConnect
 # https://github.com/andyholmes/gnome-shell-extension-gsconnect/wiki
