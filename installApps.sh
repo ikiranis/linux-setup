1117f04272858f507efca5487dcaeda696c7bd75
Yiannis Kiranis <50238022+ikiranis@users.noreply.github.com>
2019-10-15 02:51:07 +0300

    Update

--- installApps.sh
+++ installApps.sh
@@ -13,3 +13,17 @@ sudo gdebi google-chrome-stable_current_amd64.deb
 # Install Skype
 wget https://go.skype.com/skypeforlinux-64.deb
 sudo gdebi skypeforlinux-64.deb
+
+# Install Dropbox
+ wget https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2019.02.14_amd64.deb
+ sudo gdebi download\?dl\=packages%2Fubuntu%2Fdropbox_2019.02.14_amd64.deb 
+
+# Install LibreOffice
+sudo add-apt-repository ppa:libreoffice/ppa
+sudo apt update
+sudo apt install libreoffice
+
+# Install kdenlive
+sudo add-apt-repository ppa:kdenlive/kdenlive-stable
+sudo apt-get update
+sudo apt install kdenlive
